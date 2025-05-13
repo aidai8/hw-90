ca87e62a5e966dddb15a2fd455ef019726e2e4c1
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-05-13 07:17:26 +0600

    API logic created

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
+import WebSocket from "ws";
 
 const app = express();
 const wsInstance = expressWs(app);
@@ -10,6 +11,63 @@ app.use(cors());
 
 const router = express.Router();
 wsInstance.applyTo(router);
+
+const connectedClient: WebSocket[] = [];
+
+interface IncomingMessage {
+    type: string;
+    payload: any;
+}
+
+interface Pixel {
+    x: number;
+    y: number;
+    color: string;
+}
+
+const savedPixels: Pixel[] = [];
+
+router.ws('/canvas', (ws, req) => {
+    console.log('Client connected');
+    connectedClient.push(ws);
+    console.log('Total clients: ' + connectedClient.length);
+
+    ws.send(JSON.stringify({
+        type: 'INIT_PIXELS',
+        payload: savedPixels
+    }));
+
+    ws.on('message', (message) => {
+        try {
+            const decodedMessage = JSON.parse(message.toString()) as IncomingMessage;
+
+            if (decodedMessage.type === 'DRAW_PIXELS') {
+                const newPixels = decodedMessage.payload as Pixel[];
+
+                savedPixels.push(...newPixels);
+
+                connectedClient.forEach(clientWS => {
+                    if (clientWS.readyState === WebSocket.OPEN) {
+                        clientWS.send(JSON.stringify({
+                            type: 'NEW_PIXELS',
+                            payload: newPixels
+                        }));
+                    }
+                });
+            }
+
+        } catch (e) {
+            ws.send(JSON.stringify({error: 'Invalid message'}));
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        const index = connectedClient.indexOf(ws);
+        connectedClient.splice(index, 1);
+        console.log('Total connections: ' + connectedClient.length);
+    });
+});
 
 
 app.use(router);
